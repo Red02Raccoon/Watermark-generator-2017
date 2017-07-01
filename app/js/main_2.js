e31211c118473144537093f0705aacf70c71b081
redraccoon <red02raccoon@gmail.com>
2017-07-01 23:36:42 +0300

    0.0.3

--- app/js/main_2.js
+++ app/js/main_2.js
@@ -60,7 +60,62 @@ function setMainImg(file) {
 
 
  function setWatermark(file) {
- 		alert("еще нету");
+ 		var  reader = new FileReader();
+ 		reader.onload = function(e) {
+            var secondImg = document.createElement('img'),
+                containerWidth = document.querySelector(".mainImgContainer").offsetWidth,
+                containerHeight = document.querySelector(".mainImgContainer").offsetHeight,
+                wrapWidth = document.querySelector(".watermark__area").offsetWidth,
+                wrapHeight = document.querySelector(".watermark__area").offsetHeight,
+                ratioImg,
+                imgPosX,
+                imgPosY,
+                secondImgWidth,
+                secondImgHeight;
+
+
+
+            secondImg.onload = function() {
+                var secondImgWidthStart = this.width,
+                    secondImgHeightStart = this.height;
+
+                ratioImg = secondImgWidthStart / secondImgHeightStart;
+
+                if (secondImgWidthStart > secondImgHeightStart) {
+                                        console.log("no");
+                    secondImgWidth = containerWidth / 3;
+                    secondImgHeight = secondImgWidth / ratioImg;
+
+                    secondImg.setAttribute("width", secondImgWidth);
+                    secondImg.setAttribute("height", secondImgHeight);
+
+                    imgPosY = (wrapHeight - containerHeight) / 2;
+                    secondImg.style.top = imgPosY+'px';
+console.log(secondImgWidth);
+console.log(secondImgHeight);
+
+                } else if (secondImgWidthStart < secondImgHeightStart){
+                    console.log("yes");
+                    secondImgHeight = containerHeight / 3;
+                    secondImgWidth = secondImgHeight * ratioImg;
+console.log(secondImgWidth);
+console.log(secondImgHeight);
+console.log(containerWidth);
+console.log(secondImgWidth);
+                    secondImg.setAttribute("width", secondImgWidth);
+                    secondImg.setAttribute("height", secondImgHeight);
+
+                    imgPosX = (wrapWidth - containerWidth) / 2;
+                    secondImg.style.left = imgPosX+'px';
+
+                };
+            };
+
+            secondImg.src = e.target.result;
+            secondImg.className = "secondImg";
+            document.querySelector(".secondImgContainer").appendChild(secondImg);
+        };
+        reader.readAsDataURL(file);
  };
 
 
