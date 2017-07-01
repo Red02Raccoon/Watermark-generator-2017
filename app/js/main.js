9fc1f1b9b3470657cd5b35808efafa1dc949db87
redraccoon <red02raccoon@gmail.com>
2017-07-01 13:30:06 +0300

    загрузка главной картинки через канвас

--- app/js/main.js
+++ app/js/main.js
@@ -1,29 +1,58 @@
 function pushImg_1(evt) {
     var file = document.querySelector('#fileToChange').files[0];
-		read(file, 300, 300);
-    	document.getElementById('fileToTop').setAttribute('onclick', 'return true');
+		read(file);
+        document.getElementById('fileToTop').setAttribute('onclick', 'return true');
+    	document.querySelector('.settings__button--noactive').classList.toggle("settings__button--noactive");
  };
 
  function pushImg_2(evt) {
     var file = document.querySelector('#fileToTop').files[0];
-   		read(file, 200, 200);
+   		alert("Еще не сделала...работаю");
  };
 
 
- function read(file, width, height) {
+ function read(file) {
  		var  reader = new FileReader();
 
     	reader.onload = function(event) {
     		var dataURI = event.target.result,
+
     		    canvas = document.querySelector('.watermark__canvas'),
+                canvasWidth = document.querySelector('.watermark__area').offsetWidth,
+                canvasHeight = document.querySelector('.watermark__area').offsetHeight,
     			ctx = canvas.getContext('2d'),
-    			img = new Image();
+    			img = new Image(),
+                ratioImg,
+                widthImg,
+                imgPosX,
+                imgPosY,
+                heightImg;
+
+                canvas.setAttribute("width", canvasWidth);
+                canvas.setAttribute("height", canvasHeight);
 
 			img.onload = function(){
-				ctx.drawImage(img, 0, 0, width, height);
+
+                widthImg = this.width;
+                heightImg = this.height;
+
+                console.log(widthImg+'x'+heightImg); 
+
+                ratioImg = widthImg / heightImg;
+                widthImgCanvas = canvasWidth;
+                heightImgCanvas = widthImgCanvas / ratioImg;
+
+                console.log(widthImgCanvas+'x'+heightImgCanvas); 
+
+                imgPosX = canvas.width / 2 - widthImgCanvas / 2;
+                imgPosY = canvas.height / 2 - heightImgCanvas / 2;
+
+				ctx.drawImage(img, imgPosX, imgPosY, widthImgCanvas, heightImgCanvas);
 			};
+
 			img.src = dataURI;
     	};
+
     	reader.readAsDataURL(file);
  };
 
