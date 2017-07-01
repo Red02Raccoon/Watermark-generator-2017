819415e8d3deb923915b03fc74c846b9e20da29a
Andrey <andrey@appsote.co.il>
2017-07-01 21:57:28 +0300

    drop main img

--- app/js/main.js
+++ app/js/main.js
@@ -39,4 +39,88 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 
 
 //////
-$
+
+let $globalArea = $('.watermark__area'),
+    fileUploadContainer = $('.watermark__area'),
+    isDragging = false,
+    dragOverClass = 'dragover';
+
+$globalArea
+    .on('dragover', dragOverHandler)
+    .on('dragleave', dragleaveHandler)
+    .on('drop', dropHandler);
+
+function dragOverHandler (e){
+    e.stopPropagation();
+    e.preventDefault();
+
+    if (!isDragging) {
+        fileUploadContainer.addClass(dragOverClass);
+        isDragging = true;
+    }
+};
+
+function dragleaveHandler(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    console.log('dragleave');
+    if (isDragging) {
+        console.log('dragleave1');
+        fileUploadContainer.removeClass(dragOverClass);
+        isDragging = false;
+    }
+};
+
+function dropHandler(e) {
+    if (e.originalEvent.dataTransfer && e.originalEvent.dataTransfer.files) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        dropSelectFile(e.originalEvent.dataTransfer.files);
+
+        if (isDragging) {
+            fileUploadContainer.removeClass(dragOverClass);
+            isDragging = false;
+        }
+    }
+};
+
+function dropSelectFile(files){
+    if (files && files[0]) {
+        for (var i = 0; i < files.length; i++) {
+            if (isImageFile(files[i])) {
+                var file = files[i];
+
+                setImage(file);
+            }
+        }
+    }
+};
+
+function setImage(file) {
+    // $scope.files.push(file);
+     var reader = new FileReader();
+    reader.onload = function (e) {
+
+        var uri = e.target.result;
+
+        let $mainImgPrev = $('.main-img-prev');
+
+        $mainImgPrev.attr("src", uri).addClass('main-img-prev_show');
+
+        // $rootScope.safeApply(function () {
+        //     $scope.previewImages.push(uri);
+        //
+        // });
+    };
+
+    reader.readAsDataURL(file);
+};
+
+function isImageFile(file) {
+    if (file.type.indexOf('image/') === 0) {
+        return true
+    }
+
+    return false;
+};
